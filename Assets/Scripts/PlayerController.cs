c64b8cc729587a45296e761fb207794aa6658b81
Julian DUPUIS <julian.dupuis@bordeaux-inp.fr>
2020-11-01 17:36:33 +0100

    fix attack + jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -58,7 +58,6 @@ public class PlayerController : MonoBehaviour
             if (actions[lastAction] == Action.Jump && !jump && IsGrounded())
             {
                 jump = true;
-                lastAction = (lastAction + 1) % 2;
             }
         }
 
